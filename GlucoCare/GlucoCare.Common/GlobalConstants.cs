80405fa63d617256aff835cb2caf3a7fff402e72
Velyana Dimova <70020646+villdimova@users.noreply.github.com>
2021-05-14 12:16:53 +0300

    Delete GlucoCare directory

--- GlucoCare/GlucoCare.Common/GlobalConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GlucoCare.Common
-{
-    public static class GlobalConstants
-    {
-        public const string SystemName = "GlucoCare";
-
-        public const string AdministratorRoleName = "Administrator";
-    }
-}
--- GlucoCare/Web/GlucoCare.Web.ViewModels/Administration/Dashboard/IndexViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace GlucoCare.Web.ViewModels.Administration.Dashboard
-{
-    public class IndexViewModel
-    {
-        public int SettingsCount { get; set; }
-    }
-}
